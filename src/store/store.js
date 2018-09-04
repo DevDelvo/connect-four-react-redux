62883b48fff89b4ff8eac64e4a2df3e864bdb8a6
DevDelvo <kevinbdelvo@gmail.com>
2018-09-04 15:11:04 -0400

    added row and diagonal win checks

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,109 @@
 import { createStore } from 'redux';
 import { DROP_CHECKER, RESET_GAME, GAME_WINNER } from './actions'
 
+const checkWinner = (row, col, board, currentTurn) => {
+    let winningCol = checkColumn(col, board, currentTurn);
+    let winningRow = checkRow(row, board, currentTurn);
+    let leftDiagonalWinner = checkLeftDiagonal(row, col, board, currentTurn);
+    let rightDiagonalWinner = checkRightDiagonal(row, col, board, currentTurn);
+
+    let winner = winningCol || winningRow || leftDiagonalWinner || rightDiagonalWinner
+    if (winner) {
+        return currentTurn
+    }
+    // return winner;
+}
+
+const checkColumn = (column, board, currentTurn) => {
+    let count = 0;
+    let winPositions = [];
+    for (let i = 0; i < board[column].length; i++) {
+        if (board[column][i] === currentTurn) {
+            count++;
+            winPositions.push({row: i, col: column});
+            if (count === 4) {
+                return winPositions;
+            }
+        } else {
+            count = 0;
+            winPositions = [];
+        }
+    }
+}
+
+const checkRow = (row, board, currentTurn) => {
+    let count = 0;
+    let winPositions = [];
+    for (let i = 0; i < 6; i++) {
+        if (board[i][row] === currentTurn) {
+            count++;
+            winPositions.push({row: row, col: i});
+            if (count === 4) {
+                return winPositions;
+            }
+        } else {
+            count = 0;
+            winPositions = [];
+        }
+    }
+}
+
+const checkLeftDiagonal = (row, column, board, currentTurn) => {
+    let count = 0;
+    let winPositions = [];
+    let startRow = row > column ? row -column : 0;
+    let startCol = column > row ? column - row : 0;
+
+    for (let i = 0; i < 6; i++) {
+        let r = startRow + i;
+        let c = startCol + i;
+        if (r >= 6 || c >= 7) {
+            break;
+        }
+
+        if (board[r][c] === currentTurn) {
+            count++;
+            winPositions.push({row: r, col: c});
+
+            if (count === 4) {
+                console.log('left diagonal winner is', currentTurn);
+                return winPositions;
+            }
+        } else {
+            count = 0;
+            winPositions = [];
+        }
+    }
+}
+
+const checkRightDiagonal = (row, column, board, currentTurn) => {
+    let count = 0;
+    let winPositions = [];
+    let startRow = row + column <= 5 ? row + column : 5;
+    let startCol = row + column <= 5 ? 0 : (row + column) - startRow;
+
+    for (let i = 0; i < 6; i++) {
+        let r = startRow - i;
+        let c = startCol + i;
+        if (r < 0 || c >= 7) {
+            break;
+        }
+
+        if (board[r][c] === currentTurn) {
+            count++;
+            winPositions.push({row: r, col: c});
+
+            if (count === 4) {
+                console.log('right diagonal winner is', currentTurn);
+                return winPositions;
+            }
+        } else {
+            count = 0;
+            winPositions = [];
+        }
+    }
+}
+
 const board = [
     [], //col 1
     [], //col 2
@@ -18,111 +121,6 @@ const initialState = {
     winner: false,
 };
 
-const checkWinner = (row, col, board, currentTurn) => {
-    let winningCol = checkColumn(col, board, currentTurn);
-    // let winningRow = checkRow(row);
-    // let leftDiagonalWinner = checkLeftDiagonal(row, col);
-    // let rightDiagonalWinner = checkRightDiagonal(row, col);
-
-    // let winner = winningCol || winningRow || leftDiagonalWinner || rightDiagonalWinner
-    let winner = winningCol
-    console.log(winner)
-    return winner;
-}
-
-const checkColumn = (column, board, currentTurn) => {
-    let count = 0;
-    let winPositions = [];
-    for (let i = 0; i < board[column].length; i++) {
-        if (board[column][i] === currentTurn) {
-            count++;
-            winPositions.push({row: i, col: column});
-            if (count === 4) {
-                console.log('col winner is', currentTurn);
-                return winPositions;
-            }
-        } else {
-            count = 0;
-            winPositions = [];
-        }
-    }
-}
-
-// const checkRow = (row) => {
-//     let count = 0;
-//     let winPositions = [];
-//     for (let i = 0; i < 6; i++) {
-//         if (this.props.board[i][row] === this.props.currentTurn) {
-//             count++;
-//             winPositions.push({row: row, col: i});
-//             if (count === 4) {
-//                 console.log(winPositions)
-//                 console.log('row winner is', this.props.currentTurn);
-//                 return winPositions;
-//             }
-//         } else {
-//             count = 0;
-//             winPositions = [];
-//         }
-//     }
-// }
-
-// const checkLeftDiagonal = (row, column) => {
-//     let count = 0;
-//     let winPositions = [];
-//     let startRow = row > column ? row -column : 0;
-//     let startCol = column > row ? column - row : 0;
-
-//     for (let i = 0; i < 6; i++) {
-//         let r = startRow + i;
-//         let c = startCol + i;
-//         if (r >= 6 || c >= 7) {
-//             break;
-//         }
-
-//         if (this.props.board[r][c] === this.props.currentTurn) {
-//             count++;
-//             winPositions.push({row: r, col: c});
-
-//             if (count === 4) {
-//                 console.log('left diagonal winner is', this.props.currentTurn);
-//                 return winPositions;
-//             }
-//         } else {
-//             count = 0;
-//             winPositions = [];
-//         }
-//     }
-// }
-
-// const checkRightDiagonal = (row, column) => {
-//     let count = 0;
-//     let winPositions = [];
-//     let startRow = row + column <= 5 ? row + column : 5;
-//     let startCol = row + column <= 5 ? 0 : (row + column) - startRow;
-
-//     for (let i = 0; i < 6; i++) {
-//         let r = startRow - i;
-//         let c = startCol + i;
-//         if (r < 0 || c >= 7) {
-//             break;
-//         }
-
-//         if (this.props.board[r][c] === this.props.currentTurn) {
-//             count++;
-//             winPositions.push({row: r, col: c});
-
-//             if (count === 4) {
-//                 console.log('right diagonal winner is', this.props.currentTurn);
-//                 return winPositions;
-//             }
-//         } else {
-//             count = 0;
-//             winPositions = [];
-//         }
-//     }
-// }
-
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DROP_CHECKER:
@@ -139,11 +137,17 @@ const reducer = (state = initialState, action) => {
                 } else { // if the column isnt full
                     board[action.payload] = col; // update column with new tile
                     const row = board[action.payload].length - 1;
-                    const column = board[row].length;
+                    const column = action.payload;
                     console.log('player: ', tile, 'row: ', row, 'col: ', column)
-                    // console.log('this be da board',board)
-                    checkWinner(row, column, board, state.currentTurn)
-                    
+                    if (checkWinner(row, column, board, state.currentTurn)) {
+                        const winner = checkWinner(row, column, board, state.currentTurn)
+                        console.log('winner', checkWinner(row, column, board, state.currentTurn))
+                        return {
+                            board: board,
+                            gameMessage: `${winner} is the winner!!!`,
+                            winner: true,
+                        }
+                    }
                     return {
                         currentTurn: state.currentTurn === 'red' ? 'black' : 'red', //changes player turn
                         gameMessage: state.currentTurn === 'red' ? 'It is black\'s turn!' : 'It is red\'s turn!',
